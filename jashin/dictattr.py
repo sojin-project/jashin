f79eb9cb505c84a8bff34a8fc1e341afeb6d207f
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-06-06 13:44:47 +0900

    improve list member

--- jashin/dictattr.py
+++ jashin/dictattr.py
@@ -12,8 +12,12 @@ from typing import (
     Tuple,
     Union,
     List,
+    MutableSequence,
+    Iterable,
+    overload
 )
 
+import collections.abc
 
 from .omit import OMIT
 
@@ -103,22 +107,135 @@ class DictAttr(DictAttrBase[F]):
         self._set(instance, value)
 
 
+
+
+_T = TypeVar("_T")
+#class UserList(MutableSequence[_T]):
+#    data: List[_T]
+#    def __init__(self, initlist: Optional[Iterable[_T]] = ...) -> None: ...
+#    def __lt__(self, other: object) -> bool: ...
+#    def __le__(self, other: object) -> bool: ...
+#    def __gt__(self, other: object) -> bool: ...
+#    def __ge__(self, other: object) -> bool: ...
+#    def __contains__(self, item: object) -> bool: ...
+#    def __len__(self) -> int: ...
+#    @overload
+#    def __getitem__(self, i: int) -> _T: ...
+#    @overload
+#    def __getitem__(self, i: slice) -> MutableSequence[_T]: ...
+#    @overload
+#    def __setitem__(self, i: int, o: _T) -> None: ...
+#    @overload
+#    def __setitem__(self, i: slice, o: Iterable[_T]) -> None: ...
+#    def __delitem__(self, i: Union[int, slice]) -> None: ...
+#    def __add__(self: _S, other: Iterable[_T]) -> _S: ...
+#    def __iadd__(self: _S, other: Iterable[_T]) -> _S: ...
+#    def __mul__(self: _S, n: int) -> _S: ...
+#    def __imul__(self: _S, n: int) -> _S: ...
+#    def append(self, item: _T) -> None: ...
+#    def insert(self, i: int, item: _T) -> None: ...
+#    def pop(self, i: int = ...) -> _T: ...
+#    def remove(self, item: _T) -> None: ...
+#    def clear(self) -> None: ...
+#    def copy(self: _S) -> _S: ...
+#    def count(self, item: _T) -> int: ...
+#    def index(self, item: _T, *args: Any) -> int: ...
+#    def reverse(self) -> None: ...
+#    def sort(self, *args: Any, **kwds: Any) -> None: ...
+#    def extend(self, other: Iterable[_T]) -> None: ...
+
+class _AttrList(MutableSequence[_T]):
+    data: List[Any]
+    funcs: Tuple[Optional[Getter[_T]], Optional[Setter[_T]]]
+
+    def __init__(self, funcs:Tuple[Optional[Getter[_T]], Optional[Setter[_T]]], data:List[Any])->None:
+        self.func = funcs
+        self.data = data
+
+    def _from_dict(self, o: Any)->_T:
+        getter = self.funcs[0]
+        if getter:
+            return getter(o)
+        else:
+            return cast(_T, o)
+
+    def _from_dict_seq(self, o: Iterable[Any])->MutableSequence[_T]:
+        getter = self.funcs[0]
+        if getter:
+            return [getter(i) for i in o]
+        else:
+            return list(o)
+
+    def _to_dict(self, o: _T)->Any:
+        setter = self.funcs[1]
+        if setter:
+            return setter(o)
+        else:
+            return o
+
+    def _to_dict_seq(self, o: Iterable[_T])->MutableSequence[_T]:
+        setter = self.funcs[1]
+        if setter:
+            return [setter(i) for i in o]
+        else:
+            return list(o)
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    @overload
+    def __getitem__(self, i: int) -> _T: ...
+
+    @overload
+    def __getitem__(self, i: slice) -> MutableSequence[_T]: ...
+
+    def __getitem__(self, i:Union[int, slice])->Union[_T, MutableSequence[_T]]:
+        if isinstance(i, slice):
+            return self._from_dict_seq(self.data[i])
+        else:
+            return self._from_dict(self.data[i])
+
+    @overload
+    def __setitem__(self, i: int, item: _T) -> None: ...
+
+    @overload
+    def __setitem__(self, i: slice, item: Iterable[_T]) -> None: ...
+
+    def __setitem__(self, i:Union[int, slice], item:Union[_T, Iterable[_T]])->None:
+        if isinstance(i, slice):
+            if not isinstance(item, Iterable):
+                raise TypeError('can only assign an iterable')
+            self.data[i] = self._to_dict_seq(item)
+        else:
+            self.data[i] = self._to_dict(cast(_T, item))
+
+    def __delitem__(self, i:Union[int, slice])->None:
+        del self.data[i]
+
+    def __repr__(self)->str:
+        return repr(self.data)
+
+    def append(self, item:_T)->None:
+        self.data.append(self._to_dict(item))
+
+    def insert(self, i:int, item:_T)->None:
+        self.data.insert(i, self._to_dict(item))
+
+    def extend(self, other:Iterable[_T])->None:
+        self.data.extend(self._to_dict_seq(other))
+
+
+
+
 class DictAttrList(DictAttrBase[F]):
     def __get__(self, instance: Any, owner: type) -> Sequence[F]:
-        getter, value = self._get_value(instance, owner)
-        if not getter:
-            return cast(Sequence[F], value)
-
-        return [getter(v) for v in value]
+        _, value = self._get_value(instance, owner)
+        return _AttrList[F](self.funcs, value)
 
     def __set__(self, instance: Any, value: Sequence[F]) -> None:
         assert self.name, "Field name is not provided"
 
         setter = self.funcs[1]
-        values: List[Any]
-
-        data = self._get_dict(instance)
-
         if setter:
             data[self.name] = [setter(v) for v in value]
         else:
